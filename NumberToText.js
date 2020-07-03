a62476bb09818217a84869a8efac440fabea8f05
Lasse Mejlvang Tvedt <himself@lassemt.com>
2020-07-03 16:50:16 +0200

    Cleaned up

--- NumberToText.js
+++ NumberToText.js
@@ -1,21 +1,17 @@
 
 // SparkAr module
-const s = require("Scene");
-const p = require("Patches");
+const Scene = require("Scene");
+const Patches = require("Patches");
 
 (async function () {
+  //importing the text elements
+  let txtScore = await Scene.root.findFirst("2dText0");
 
-//importing the text elements
-let Text = await s.root.findFirst("2dText0");
-
-//geting the score toScript value text 
-Text.text = p.outputs.getScalar("score").then(obj => {
-
-obj.monitor().subscribe(o => {
-
-//return the score as a string
-return o.newValue.toString();
-
-});
-});
+  //geting the score toScript value text 
+  txtScore.text = Patches.outputs.getScalar("score").then(patchScore => {
+    patchScore.monitor().subscribe(evt => {
+      //return the score as a string
+      return evt.newValue.toString();
+    });
+  });
 })();

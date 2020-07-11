6ed4bc6d7598188e24c655bd71185a7a0eb02fb2
Lasse Mejlvang Tvedt <himself@lassemt.com>
2020-07-11 14:58:18 +0200

    Cleaned up

--- ColorSpaceConvert.js
+++ ColorSpaceConvert.js
@@ -1,5 +1,5 @@
-const T = require('Time');
-const M = require('Materials');
+const Time = require('Time');
+const Mat = require('Materials');
 const R = require('Reactive');
 const Shaders = require('Shaders');
 
@@ -11,32 +11,25 @@ const colorspaceconfig = {
 
 //Find objects
 Promise.all([
-    M.findFirst('material0')
+    Mat.findFirst('material0')
 ]).then(loadasset);
 
 //load objects
 function loadasset(obj){
-    let mat = obj[0];
+    const mat = obj[0];
     let hue = 0;
     let color;
 
-    T.setInterval(()=>{
-        //set a random hue value from 0-1 for every 500ms
+    Time.setInterval(()=>{
+        // Set a random hue value from 0-1 for every 500ms
         hue = Math.random()
-        //convert HSV to RGB
-        color = Shaders.colorSpaceConvert(R.pack3(hue,1,1),colorspaceconfig);
-        //Pack to vec4 with alpha 1
-        color = R.pack4(color.x, color.y,color.z,1)
+        // Convert HSV to RGB
+        color = Shaders.colorSpaceConvert(R.pack3(hue, 1, 1), colorspaceconfig);
+        // Pack to vec4 with alpha 1
+        color = R.pack4(color.x, color.y, color.z, 1)
 
-        //set textureslot and assign to material
+        // Set textureslot and assign to material
         const textureslot = Shaders.DefaultMaterialTextures.DIFFUSE;
         mat.setTextureSlot(textureslot, color);
-
-    },500)
-
-    
-}
-
-
-
-;
+    }, 500);
+};

bf5408b006243f10bb95a3d0c2f7a65f041de9aa
kavin kumar <bkavin001@gmail.com>
2020-07-15 21:18:14 +0530

    Update getingChild.js

--- getingChild.js
+++ getingChild.js
@@ -1,14 +1,20 @@
-// made by kavin kumar 
-// Importing module
-const s = require("Scene");
+// code by kavin kumar
 
-//promis start 
+//this is a example of accessing child object using the parent
+//this will help you when u have children having same name with multiple parents 
+
+//  load in modules
+const s = require('Scene');
+const d = require('Diagnostics');
+
+//promise start 
+
 Promise.all([
-    // Geting the parent
-   s.root.findByPath("**/nullObject0/plane0")
- 
-]).then(result => {
-    //can access the child
-    let obj = result[0];
 
+//it goes to the object1 first and then it looks for object1 child named object2
+
+    s.root.findByPath("**/object1/object2")
+
+]).then((result) => {
+    d.log(result[0][0].name)
 });

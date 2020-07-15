824b8cda826688003dd5dacd2b26a2cd9791d398
kavin kumar <bkavin001@gmail.com>
2020-07-15 21:01:12 +0530

    Update getingChild.js

--- getingChild.js
+++ getingChild.js
@@ -4,10 +4,8 @@ const s = require("Scene");
 //promis start 
 Promise.all([
     // Geting the parent
-    s.root.findFirst("object1").then(p => {
-        //geting the child
-        return p.findFirst("object2");
-    }),
+   s.root.findByPath("**/nullObject0/plane0")
+ 
 ]).then(result => {
     //can access the child
     let obj = result[0];

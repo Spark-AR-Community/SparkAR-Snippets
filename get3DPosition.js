2258ab5046de0fd2fafaff58a19a7646c29ef9e7
Lasse Mejlvang Tvedt <himself@lassemt.com>
2020-07-16 15:54:38 +0200

    Update and rename get3dPosition.js to get3DPosition.js

--- get3DPosition.js
+++ get3DPosition.js
@@ -3,24 +3,21 @@
 //this code allow us to get the 3d position of any object and send it to patch editor
 
 //Importing modules
-const s = require("Scene");
-const p = require("Patches");
+const Scene = require("Scene");
+const Patches = require("Patches");
 
 
 Promise.all([
     // it gets the first element thats named object
-    s.root.findFirst("object")
+    Scene.root.findFirst("object")
 ]).then(result => {
-
     // result[0] contain the object 
-    let obj = result[0];
+    const obj = result[0];
 
     // need to create a from script vec 3 called objPos 
     // objects position will be sent to the objPos 
-    p.inputs.setVector("objPos", obj.transform.position);
+    Patches.inputs.setVector("objPos", obj.transform.position);
 
     //objects World Position will be sent to the vex 3 fromScript called objWorldPos
-    p.inputs.setVector("objWorldPos", obj.worldTransform.position);
-
-
+    Patches.inputs.setVector("objWorldPos", obj.worldTransform.position);
 });

485e02e4e15d0638e14f66f4f8c3b89494fc6a16
Lasse Mejlvang Tvedt <himself@lassemt.com>
2020-07-16 15:52:16 +0200

    Update and rename getingChild.js to gettingChildObjects.js

--- gettingChildObjects.js
+++ gettingChildObjects.js
@@ -4,17 +4,14 @@
 //this will help you when u have children having same name with multiple parents 
 
 //  load in modules
-const s = require('Scene');
-const d = require('Diagnostics');
+const Scene = require('Scene');
+const D = require('Diagnostics');
 
 //promise start 
 
 Promise.all([
-
-//it goes to the object1 first and then it looks for object1 child named object2
-
-    s.root.findByPath("**/object1/object2")
-
+    // it goes to the object1 first and then it looks for object1 child named object2
+    Scene.root.findByPath("**/object1/object2")
 ]).then((result) => {
-    d.log(result[0][0].name)
+    D.log(result[0][0].name)
 });

506949187340dc53b606b2b02cd379857f11e2cc
Lasse Mejlvang Tvedt <himself@lassemt.com>
2020-10-05 19:45:30 +0200

    Cleaned up spacing

--- HairColorAverage.js
+++ HairColorAverage.js
@@ -3,9 +3,9 @@ const Materials = require('Materials');
 const Segmentation = require('Segmentation');
 const Reactive = require('Reactive');
 const Shaders = require('Shaders');
+
 // Enable async/await in JS [part 1]
 (async function () {
-
     //Find Material 
     const [mat] = await Promise.all([
         Materials.findFirst('defaultMaterial0')
@@ -13,14 +13,16 @@ const Shaders = require('Shaders');
 
     let color;
     // Getting averge color of the hair 
-    color = Reactive.pack4(Segmentation.hair.averageColor.red, Segmentation.hair.averageColor.green, Segmentation.hair.averageColor.blue, Segmentation.hair.averageColor.alpha)
+    color = Reactive.pack4(
+        Segmentation.hair.averageColor.red, 
+        Segmentation.hair.averageColor.green, 
+        Segmentation.hair.averageColor.blue, 
+        Segmentation.hair.averageColor.alpha
+    );
 
     // Getting the texture Slot
     const textureslot = Shaders.DefaultMaterialTextures.DIFFUSE;
 
     // Setting the color in the Texture slot
     mat.setTextureSlot(textureslot, color);
-
-
-
 })();

33941d7d4fe88b22533648483d644c2dcb6658de
Shivek <shivek@phixyl.com>
2020-12-05 23:38:13 +0100

    Gallery Texture Dimensions

---
+++ GalleryTextureDimensions.js
@@ -0,0 +1,21 @@
+//==============================================================================
+// The following example demonstrates how to get the width and height of the
+// Gallery Texture and pass them to Patch Editor
+//
+// Project setup:
+// - Add a gallery texture asset (Assets > Add asset > Gallery Texture) and
+//   keep its default name. You can change it later.
+// - Inspect the script asset and add two From Script variables:
+//   GalleryTextureWidth and GalleryTextureHeight
+// - Click the 'Create Producer Patch' button which is below the variables
+//==============================================================================
+
+const Diagnostics = require('Diagnostics');
+const Textures = require('Textures');
+const Patches = require('Patches');
+
+(async function() {
+  const galleryTexture = await Textures.findFirst('galleryTexture0');
+  await Patches.inputs.setScalar('GalleryTextureWidth', galleryTexture.width);
+  await Patches.inputs.setScalar('GalleryTextureHeight', galleryTexture.height);
+})();

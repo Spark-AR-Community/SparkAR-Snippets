0baf3f1f026222c5dc9ea287d7b92d8ed3c8a4fc
Balraj <64496240+bb1100@users.noreply.github.com>
2021-05-03 20:10:56 +0100

    Create tapToChangeMat.js

---
+++ tapToChangeMat.js
@@ -0,0 +1,44 @@
+// Script to 'tap to change' an object's material
+// Rewritten for Spark AR v86+ by Balraj Bains 2021
+// Original author for pre v86 Bridget Walsh Clair :)
+
+
+
+// Load modules
+const Scene = require('Scene');
+const Materials = require('Materials');
+const TouchGestures = require('TouchGestures');
+const Instruction = require('Instruction');
+
+
+// Main async function pt1
+(async function() {
+    // Set the instruction to show 'Tap to change'
+    Instruction.bind(true, 'tap_to_change');
+
+    // Access materials
+    const mats = await Promise.all([ 
+        Materials.findFirst('green'),
+        Materials.findFirst('red'),
+        Materials.findFirst('blue')
+    ]);
+
+    // Access scene objects
+    const objectToChange = await Scene.root.findFirst('faceMesh0');
+
+
+    // Assign the value of the current index in the mats array to the specified scene object
+    let matIndex = 0;
+    objectToChange.material = mats[matIndex];
+    TouchGestures.onTap().subscribe(toggleMat);
+
+    function toggleMat() {
+        matIndex++;
+        if (matIndex >= mats.length) {
+            matIndex = 0;
+        }
+        objectToChange.material = mats[matIndex];
+    };
+    
+// Enables async/await pt2
+})();

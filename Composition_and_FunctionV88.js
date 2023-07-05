43ba659cba8f8a07bdf2ef22a367d414554029e9
Tomás Pietravallo <pietravallo21tomas@gmail.com>
2023-07-04 22:36:34 -0300

    fix: setTextureSlot breaking API change

--- Composition_and_FunctionV88.js
+++ Composition_and_FunctionV88.js
@@ -4,30 +4,31 @@ const S = require('Shaders');
 const Tex = require('Textures');
 const R = require('Reactive');
 const M = require('Materials');
+
 Promise.all([
 	Tex.findFirst('cameraTexture0'),
     M.findFirst('user_mat'),
 ]).then(onReady);
 
 function onReady(assets) {
-const diffuse = assets[0];
-const defaultMaterial = assets[1];
-const diffuseTexture = diffuse.signal;
+    const diffuse = assets[0];
+    const defaultMaterial = assets[1];
+    const diffuseTexture = diffuse.signal;
 
-const vtx_uv = S.vertexAttribute({ variableName: S.VertexAttribute.TEX_COORDS });
-const vtx_plus_one = R.add(vtx_uv, R.pack2(1,1)); // runs in vtx shader
-const frag_uv = S.fragmentStage(vtx_uv);
-const frag_plus_one = R.add(frag_uv, R.pack2(1,1)); // runs in fragment shader
-const diffuseColor = S.textureSampler(diffuseTexture, vtx_uv);
+    const vtx_uv = S.vertexAttribute({ variableName: S.VertexAttribute.TEX_COORDS });
+    const vtx_plus_one = R.add(vtx_uv, R.pack2(1,1)); // runs in vtx shader
+    const frag_uv = S.fragmentStage(vtx_uv);
+    const frag_plus_one = R.add(frag_uv, R.pack2(1,1)); // runs in fragment shader
+    const diffuseColor = S.textureSampler(diffuseTexture, vtx_uv);
 
-//sm - composition
-// const func_uv = S.functionVec2();
-// const func_plus_one = R.add(func_uv, R.pack2(1,1)); //runs in fragment shader 
-// const diffuseColor = S.composition(diffuseTexture, func_uv); //order of samples determined by chain usage
+    // sm - composition
+    // const func_uv = S.functionVec2();
+    // const func_plus_one = R.add(func_uv, R.pack2(1,1)); //runs in fragment shader 
+    // const diffuseColor = S.composition(diffuseTexture, func_uv); //order of samples determined by chain usage
 
-//sm - material
-const diffuseTexSlot = S.DefaultMaterialTextures.DIFFUSE;
-defaultMaterial.setTextureSlot(diffuseColor, { textureSlotName: diffuseTexSlot });
+    // sm - material
+    const diffuseTexSlot = S.DefaultMaterialTextures.DIFFUSE;
+    defaultMaterial.setTextureSlot(diffuseTexSlot, diffuseColor);
 }
 
 // by  Sohail Mehra and Noland Chaliha

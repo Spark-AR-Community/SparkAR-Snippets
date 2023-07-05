b03bbd78a5402df60efc4a37ba0aaf1d757e312e
Tomás Pietravallo <pietravallo21tomas@gmail.com>
2023-07-04 22:38:44 -0300

    fix: API/ data types, missing capability
    Lack of `const Reactive = require("Reactive");` triggered an error

--- get3DPosition.js
+++ get3DPosition.js
@@ -1,23 +1,23 @@
 // code by RBKavin
 
-//this code allow us to get the 3d position of any object and send it to patch editor
+// this code allow us to get the 3d position of any object and send it to patch editor
 
-//Importing modules
+// Importing modules
 const Scene = require("Scene");
 const Patches = require("Patches");
-
+const Reactive = require("Reactive");
 
 Promise.all([
     // it gets the first element thats named object
     Scene.root.findFirst("object")
 ]).then(result => {
-    // result[0] contain the object 
+    // result[0] contain the object
     const obj = result[0];
 
     // need to create a from script vec 3 called objPos 
     // objects position will be sent to the objPos 
-    Patches.inputs.setVector("objPos", obj.transform.position);
+    Patches.inputs.setPoint("objPos", obj.transform.position);
 
     //objects World Position will be sent to the vex 3 fromScript called objWorldPos
-    Patches.inputs.setVector("objWorldPos", obj.worldTransform.position);
+    Patches.inputs.setPoint("objWorldPos", obj.worldTransform.position);
 });
